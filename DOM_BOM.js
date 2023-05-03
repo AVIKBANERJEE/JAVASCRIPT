f0dcbc57ea2b15b18f7fd14c4bd3acfe11e6816d
AVIKBANERJEE <84579218+AVIKBANERJEE@users.noreply.github.com>
2023-05-03 22:06:39 +0530

    Create DOM_BOM.js

---
+++ DOM_BOM.js
@@ -0,0 +1,10 @@
+console.log(window)
+//OR //Both are same
+window.console.log(window)
+//DOM
+console.log(document.body)
+document.body.style.background = "yellow"
+/*We can change all the properties of CSS and HTMl using java
+script*/
+//location.href="https:\\www.youtube.com"
+//Write it on console window
