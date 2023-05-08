d345a4ee2ca8ca850176b8a08aec3f510d9f236f
AVIKBANERJEE <84579218+AVIKBANERJEE@users.noreply.github.com>
2023-05-08 16:52:25 +0530

    Create Parent_and_Sibling.js

---
+++ Parent_and_Sibling.js
@@ -0,0 +1,5 @@
+console.log(document.body.firstChild)
+let a=document.body.firstChild
+console.log(a.parentNode)//it returns node and if there is no element then also it will return document//
+console.log(a.parentElement)//it returns null when there is no element//
+console.log(a.firstChild.nextSibling)
